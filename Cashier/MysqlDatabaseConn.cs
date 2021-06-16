d5a252f07fd2b762682cfee4853c7e4b2284da6d
属语 <1440837150@qq.com>
2021-06-16 10:43:04 +0800

    mess

---
+++ Cashier/MysqlDatabaseConn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier
+{
+    class MysqlDatabaseConn
+    {
+        public static String connString = "server=121.43.148.246;User Id=root;password=root;Database=canyin";
+    }
+}
---
+++ Cashier/WaiterWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cashier
+{
+    /// <summary>
+    /// WaiterWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class WaiterWindow : Window
+    {
+        public WaiterWindow()
+        {
+            InitializeComponent();
+            frame.Source = new Uri("WaiterMenuPage.xaml", UriKind.Relative);
+        }
+
+        private void textBlock2_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            String choice = btn.Content.ToString();
+            switch (choice)
+            {
+                case "菜单浏览":
+                    LoadWaiterMenuPage();
+                    break;
+                case "已完成订单":
+                    LoadOderCompletedPage();
+                    break;
+                case "未完成订单":
+                    LoadOderNotPage();
+                    break;
+            }
+        }
+
+        private void LoadWaiterMenuPage()
+        {
+            frame.Source = new Uri("WaiterMenuPage.xaml", UriKind.Relative);
+        }
+
+        private void LoadOderNotPage()
+        {
+            frame.Source = new Uri("OderNotPage.xaml", UriKind.Relative);
+        }
+
+        private void LoadOderCompletedPage()
+        {
+            frame.Source = new Uri("OderCompletedPage.xaml", UriKind.Relative);
+        }
+    }
+}
